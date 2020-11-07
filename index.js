95c268d3119b379dc75698d3c1389e4df7748ef6
manisha2307 <vallerimanisha2307@gmail.com>
2020-11-07 19:07:13 +0530

    seventh

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ window.addEventListener("load", () => {
     const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector(".visual");
     const colors = [
-      "#60c2d3",
-      "#d36060",
-      "#c060d3",
-      "#d3d160",
-      "#606bd3",
-      "#60c2d3"
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#606bd3",
+        "#60c2d3"
     ];
   
     pads.forEach((pad, index) => {
