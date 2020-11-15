068c34d5a7576d4cb9b81760cb82a0ca23ecbe7a
manisha2307 <vallerimanisha2307@gmail.com>
2020-11-15 13:06:27 +0530

    eight

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ window.addEventListener("load", () => {
     const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector(".visual");
     const colors = [
-        "#60d394",
-        "#d36060",
-        "#c060d3",
-        "#d3d160",
-        "#606bd3",
-        "#60c2d3"
+        "#cf3ac3",
+        "#bd0909",
+        "#3fd364",
+        "#f5f240",
+        "#690a8f",
+        "#16ceee",
     ];
   
     pads.forEach((pad, index) => {
